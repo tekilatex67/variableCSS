a85b2dd3eebf0b51eada525dd77ffe22cc41974d
Klipfou <nos_67210@hotmail.fr>
2020-04-18 15:09:02 +0200

    problems résolu

--- variables.js
+++ variables.js
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-let controls = document.querySelectorAll('input');
-const image = document.querySelector('img');
-const js = document.querySelector('.hl');
-
-controls.forEach(control => control.addEventListener('input', function(e) {
-
-    let valeur = e.target.value;
-    let element = e.srcElement.name;
-
-    if (element == "spacing") {
-        image.style.padding = valeur + "px";
-    } else if (element == "blur") {
-        image.style.filter = "blur(" + valeur + "px)";
-    } else if (element == "base") {
-        image.style.background = valeur;
-        js.style.color = valeur;
-    }
-}));
-=======
 let inputs = document.querySelectorAll('input');
 
 function handleUpdate() {
@@ -27,4 +7,3 @@ function handleUpdate() {
 }
 
 inputs.forEach(input => input.addEventListener('input', handleUpdate));
->>>>>>> 32b6130... JS
